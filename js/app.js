9297abd98542185e456de48780c0c57c65f04df2
JuanPCT <89850618+JuanPCT@users.noreply.github.com>
2022-10-07 12:01:09 -0500

    Agrega logos y estilos

--- js/app.js
+++ js/app.js
@@ -2,34 +2,45 @@
 const carrito = document.querySelector('#carrito');
 const listaCursos = document.querySelector('#lista-cursos');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
-const vaciarCarritoBtn = document.querySelector('#vaciar-carrito'); 
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+const comprar = document.querySelector('#comprar');
 let articulosCarrito = [];
 
 function mostrarCocteles() {
      let url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic';
- 
+
      fetch(url)
-         .then(response => response.json())
-         .then(data => mostrarData(data))
-         .catch(error => console.log(error));
- 
+          .then(response => response.json())
+          .then(data => mostrarData(data))
+          .catch(error => console.log(error));
+
      const mostrarData = (data) => {
-         console.log(data)
- 
-         let body = ''
-         for (let i = 0; i < data.drinks.length; i++) {
-             body += `<div class="card mt-5" style="width: 18rem;">
-                                 <img class="card-img-top" src=${data.drinks[i].strDrinkThumb} alt="Card image cap">
-                                 <div class="card-body">
-                                     <h5 class="card-title">${data.drinks[i].strDrink}</h5>
-                                     <p class="card-text">${data.drinks[i].idDrink} 20.000</p>
-                                     <a href="parcial_Index.html" class="btn btn-primary"">COMPRAR</a>
-                                 </div>
-                             </div>`
-         } 
-         document.getElementById('mostrar').innerHTML = body
+          console.log(data)
+
+          let body = ''
+          for (let i = 0; i < data.drinks.length; i++) {
+               body += `<div class="card mt-5" style="width: 18rem;">
+                                   <img class="card-img-top" src="${data.drinks[i].strDrinkThumb}" alt="Card image cap">
+                                   <div class="info-card">
+                                        <h4>${data.drinks[i].strDrink}</h4>
+                                        <p>${data.drinks[i].idDrink}</p>
+                                        <p class="precio"><span class="u-pull-right ">$20.000</span></p>
+                                        <a href="#" class="u-full-width button-primary button input agregar-carrito" data-id="1">Agregar</a>
+                                   </div>
+                              </div> `
+          }
+          //     `<div class="card mt-5" style="width: 18rem;">
+          //     <img class="card-img-top" src=${data.drinks[i].strDrinkThumb} alt="Card image cap">
+          //     <div class="card-body">
+          //         <h5 class="card-title">${data.drinks[i].strDrink}</h5>
+          //         <p class="card-text">${data.drinks[i].idDrink} 20.000</p>
+          //         <a href="#" class="u-full-width button-primary button input agregar-carrito" data-id="1">Agregar
+          //         Al Carrito</a>
+          //     </div>
+          //        </div>`
+          document.getElementById('mostrar').innerHTML = body
      }
- }
+}
 mostrarCocteles();
 
 // Listeners
@@ -38,17 +49,17 @@ cargarEventListeners();
 function cargarEventListeners() {
      // Dispara cuando se presiona "Agregar Carrito"
      listaCursos.addEventListener('click', agregarCurso);
-
      // Cuando se elimina un curso del carrito
      carrito.addEventListener('click', eliminarCurso);
-
      // Al Vaciar el carrito
      vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
-
-
+     // Ir a comprar
+     comprar.addEventListener('click', function(){
+          window.location.href = "comprar.html";
+     });
      // NUEVO: Contenido cargado
      document.addEventListener('DOMContentLoaded', () => {
-          articulosCarrito = JSON.parse( localStorage.getItem('carrito') ) || []  ;
+          articulosCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
           // console.log(articulosCarrito);
           carritoHTML();
      });
@@ -59,7 +70,7 @@ function cargarEventListeners() {
 function agregarCurso(e) {
      e.preventDefault();
      // Delegation para agregar-carrito
-     if(e.target.classList.contains('agregar-carrito')) {
+     if (e.target.classList.contains('agregar-carrito')) {
           const curso = e.target.parentElement.parentElement;
           // Enviamos el curso seleccionado para tomar sus datos
           leerDatosCurso(curso);
@@ -72,27 +83,27 @@ function leerDatosCurso(curso) {
           imagen: curso.querySelector('img').src,
           titulo: curso.querySelector('h4').textContent,
           precio: curso.querySelector('.precio span').textContent,
-          id: curso.querySelector('a').getAttribute('data-id'), 
+          id: curso.querySelector('a').getAttribute('data-id'),
           cantidad: 1
      }
 
-     if( articulosCarrito.some( curso => curso.id === infoCurso.id ) ) { 
-          const cursos = articulosCarrito.map( curso => {
-               if( curso.id === infoCurso.id ) {
+     if (articulosCarrito.some(curso => curso.id === infoCurso.id)) {
+          const cursos = articulosCarrito.map(curso => {
+               if (curso.id === infoCurso.id) {
                     let cantidad = parseInt(curso.cantidad);
                     cantidad++
-                    curso.cantidad =  cantidad;
+                    curso.cantidad = cantidad;
                     return curso;
                } else {
                     return curso;
                }
           })
           articulosCarrito = [...cursos];
-     }  else {
+     } else {
           articulosCarrito = [...articulosCarrito, infoCurso];
      }
 
-     console.log(articulosCarrito);     
+     console.log(articulosCarrito);
 
      // console.log(articulosCarrito)
      carritoHTML();
@@ -101,11 +112,11 @@ function leerDatosCurso(curso) {
 // Elimina el curso del carrito en el DOM
 function eliminarCurso(e) {
      e.preventDefault();
-     if(e.target.classList.contains('borrar-curso') ) {
+     if (e.target.classList.contains('borrar-curso')) {
           // e.target.parentElement.parentElement.remove();
           const curso = e.target.parentElement.parentElement;
           const cursoId = curso.querySelector('a').getAttribute('data-id');
-          
+
           // Eliminar del arreglo del carrito
           articulosCarrito = articulosCarrito.filter(curso => curso.id !== cursoId);
 
@@ -132,10 +143,8 @@ function carritoHTML() {
           `;
           contenedorCarrito.appendChild(row);
      });
-
      // NUEVO:
      sincronizarStorage();
-
 }
 // NUEVO: 
 function sincronizarStorage() {
@@ -143,7 +152,7 @@ function sincronizarStorage() {
 }
 // Elimina los cocteles del carrito
 function vaciarCarrito() {
-     while(contenedorCarrito.firstChild) {
+     while (contenedorCarrito.firstChild) {
           contenedorCarrito.removeChild(contenedorCarrito.firstChild);
-      }
+     }
 }
